d587b1a3db5a68e7634efb5fc0b16397768a1d50
Abdallah Ahmed <abdallah.ahmed2022222@gmail.com>
2025-04-06 11:38:39 +0200

    add validition to inputs

--- app/meals/share/page.jsx
+++ app/meals/share/page.jsx
@@ -2,7 +2,10 @@ import classes from "./page.module.css";
 import ImagePicker from "../../../components/meals/image-picker";
 import { shareMeal } from "../../../lib/actions";
 import MealsFormSubmit from "../../../components/meals/meals-form-submit";
+import { useActionState } from "react";
+
 export default function ShareMealPage() {
+  const [state, formAction] = useActionState(shareMeal, { message: "" });
   return (
     <>
       <header className={classes.header}>
@@ -12,7 +15,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -41,6 +44,7 @@ export default function ShareMealPage() {
             ></textarea>
           </p>
           <ImagePicker label="Your Image" name="image" />
+          {state.message && <p>{state.message}</p>}
           <p className={classes.actions}>
             <MealsFormSubmit />
           </p>
